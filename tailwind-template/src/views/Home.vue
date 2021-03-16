8a694f222109021714db387e72b8c9cba278b852
WhalenSITHS <38401651+WhalenSITHS@users.noreply.github.com>
2021-03-16 09:23:41 -0400

    Initial commit

---
+++ tailwind-template/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img class="mx-auto" alt="Vue logo" src="../assets/logo.png" />
+    <h1 class="text-blue-500 bg-red-500">TEST</h1>
+
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+<style lang="postcss" scoped></style>
